bc36e5f820f46dfacf08179ab967239a832291be
Daniel <danielbou@outlook.com.br>
2021-11-24 13:26:06 -0300

    Guarda Resolver para carregar a lista antes do componente

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { AnimaisService } from './../animais.service';
 import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
@@ -11,21 +12,16 @@ import { Observable } from 'rxjs';
   styleUrls: ['./lista-animais.component.css']
 })
 export class ListaAnimaisComponent implements OnInit {
-
-  animais$ !: Observable<Animais>;
+  animais !: Animais;
 
   constructor(
-    private usuarioService:UsuarioService,
-    private animaisService:AnimaisService
+    private activateRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.animais$ = this.usuarioService.retornaUsuario().pipe(
-      switchMap((usuario)=> {
-        const userName = usuario.name ?? '';
-        return this.animaisService.listaDoUsuario(userName);
-      })
-    );
+    this.activateRoute.params.subscribe((params) => {
+      this.animais = this.activateRoute.snapshot.data['animais'];
+    });
   }
 
 }
